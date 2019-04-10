b89b126a51e3cdc461ad7d81b596c44e6d529faa
Brewer <Jesse.X.Brewer.-ND@disney.com>
2019-04-10 14:09:58 -0400

    Passport auth, dashboard

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 const flash = require('connect-flash');
 const session = require('express-session');
+const passport = require('passport');
 
 const app = express();
+
+// Passport config
+require('./config/passport')(passport);
 
 // DB config
 const db = require('./config/keys').MongoURI;
@@ -28,10 +32,23 @@ app.use(session({
   saveUninitialized: true
 }));
 
-// Routes 
-app.use('/', require('./routes/index'));
+// Passportjs middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global Vars !!! Custom Middleware
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
 
 // Routes 
+app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
 
 const PORT = process.env.PORT || 5000;
